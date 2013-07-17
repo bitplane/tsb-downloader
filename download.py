3266bf65f35ffd7f5b73435d85f08d7c0c0ba8c9
Will Thompson <will@willthompson.co.uk>
2013-07-17 19:11:18 +0100

    Allow specifying user ID, from and to dates as arguments

--- download.py
+++ download.py
@@ -3,6 +3,8 @@
 Outputs a CSV, pipe it somewhere or something.
 """
 
+import argparse
+import datetime
 import getpass
 import mechanize
 
@@ -18,7 +20,7 @@ def extract(data, before, after):
     end   = data.index(after, start)
     return data[start:end]
 
-def download():
+def download(user_id, from_date, to_date):
     # a new browser and open the login page
     br = mechanize.Browser()
     br.set_handle_robots(False)
@@ -28,7 +30,7 @@ def download():
     while 'Enter Memorable Information' not in title:
         print br.title()
         br.select_form(name='frmLogin')
-        br['frmLogin:strCustomerLogin_userID'] = prompt("Enter user ID:")
+        br['frmLogin:strCustomerLogin_userID'] = str(user_id)
         br['frmLogin:strCustomerLogin_pwd']    = prompt("Enter password:", True)
         response = br.submit() # attempt log-in
         title    = br.title()
@@ -73,21 +75,30 @@ def download():
     # "Date range" as opposed to "Current view of statement"
     br['frmTest:rdoDateRange'] = ['1']
 
-    yyyy, mm, dd = prompt("Enter start date (YYYY/MM/DD): ").split('/', 2)
-    br['frmTest:dtSearchFromDate'] = [yyyy]
-    br['frmTest:dtSearchFromDate.month'] = [mm]
-    br['frmTest:dtSearchFromDate.year'] = [dd]
+    def setDate(field_name, date):
+        br[field_name] = [date.strftime('%d')]
+        br[field_name + '.month'] = [date.strftime('%m')]
+        br[field_name + '.year'] = [date.strftime('%Y')]
 
-    yyyy, mm, dd = prompt("Enter end date (YYYY/MM/DD): ").split('/', 2)
-    br['frmTest:dtSearchToDate'] = [yyyy]
-    br['frmTest:dtSearchToDate.month'] = [mm]
-    br['frmTest:dtSearchToDate.year'] = [dd]
+    setDate('frmTest:dtSearchFromDate', from_date)
+    setDate('frmTest:dtSearchToDate', to_date)
 
     response = br.submit()
     print response.read()
 
-def main():
-    download()
+def parse_date(string):
+    try:
+        yyyy, mm, dd = string.split('/', 2)
+        return datetime.date(int(yyyy), int(mm), int(dd))
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            "'%s' is not a valid date in the form YYYY/MM/DD" % string)
 
 if __name__=='__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user-id', type=int, required=True)
+    parser.add_argument('-f', '--from', type=parse_date, metavar='YYYY/MM/DD', required=True)
+    parser.add_argument('-t', '--to',   type=parse_date, metavar='YYYY/MM/DD', required=True)
+    args = parser.parse_args()
+
+    download(user_id=args.user_id, from_date=getattr(args, 'from'), to_date=args.to)
